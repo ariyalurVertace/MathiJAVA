957ecdb08bf9ec80f23e216b2b2b5de21fe64501
Sakthivel E <atttvrt001@gmail.com>
2021-11-29 17:19:51 +0530

    deleted

--- src/main/java/com/vertace/javapostgre/exceptions/EmailIdAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.vertace.javapostgre.exceptions;
-
-public class EmailIdAlreadyExistException extends Exception {
-
-    public EmailIdAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/vertace/javapostgre/exceptions/UserNameAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.vertace.javapostgre.exceptions;
-
-public class UserNameAlreadyExistException extends Exception {
-    public UserNameAlreadyExistException(String message) {
-        super(message);
-    }
-}
